05965a65f601bfbd70111f654a8c87286c0bc8dd
Aaron Cox <aaron@greymass.com>
2024-02-03 11:29:30 -0800

    Added other table clearing options

--- src/debug.cpp
+++ src/debug.cpp
@@ -22,13 +22,19 @@ drops::cleartable(const name table_name, const optional<name> scope, const optio
    const uint64_t value         = scope ? scope->value : get_self().value;
 
    // tables
-   drops::state_table _state(get_self(), value);
-   drops::drop_table  _drop(get_self(), value);
+   drops::state_table    _state(get_self(), value);
+   drops::drop_table     _drop(get_self(), value);
+   drops::stat_table     _stat(get_self(), value);
+   drops::balances_table _balances(get_self(), value);
 
    if (table_name == "drop"_n)
       clear_table(_drop, rows_to_clear);
+   else if (table_name == "balances"_n)
+      clear_table(_balances, rows_to_clear);
    else if (table_name == "state"_n)
       _state.remove();
+   else if (table_name == "stat"_n)
+      _stat.remove();
    else
       check(false, "cleartable: [table_name] unknown table to clear");
 }
