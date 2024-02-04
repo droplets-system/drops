728dffc9ec54920a983e1b402ac9baa31657fd4b
Denis Carriere <carriere.denis@gmail.com>
2024-02-04 11:03:56 -0500

    format

--- include/drops/drops.hpp
+++ include/drops/drops.hpp
@@ -29,7 +29,8 @@ static const string MEMO_RAM_SOLD_TRANSFER = "Claiming sold RAM bytes.";
 
 // feature flags
 static const bool FLAG_FORCE_RECEIVER_TO_BE_SENDER = true;
-static const bool FLAG_ENABLE_RAM_TRANSFER_ON_CLAIM = false; // not available until system contract supports `ramtransfer`
+static const bool FLAG_ENABLE_RAM_TRANSFER_ON_CLAIM =
+   false; // not available until system contract supports `ramtransfer`
 
 uint128_t combine_ids(const uint64_t& v1, const uint64_t& v2) { return (uint128_t{v1} << 64) | v2; }
 
